2bb355b918ea4ef0d139f89f569d06d40f12a163
Kaustubh Supe <69507898+supe29@users.noreply.github.com>
2023-07-25 12:59:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,21 +4,36 @@ from flask import Flask, request, jsonify, Response
 from datetime import datetime
 from time import time
 from helpers import HelperService
+from collections import Counter
+import requests
+from dateutil.parser import parse as parse_date
 
 # Setting up Flask app
 app = Flask(__name__)
 
-# Utility functions
+
+logs = []
 
 
 def add_to_log(date: datetime, url: str):
-    # TODO: add your code below...
+  
+    logs.append({"date": date.date(), "url": url})
+
     pass
 
 
 def count_break_links(links: list[str]) -> int:
-    # TODO: add your code below...
-    pass
+    
+    count = 0
+    for link in links:
+        start = time()
+        res = requests.get(link)
+        end = time() - start
+        print(link)
+        if res.status_code >= 400 or end > 60:
+            count = count + 1
+
+    return count
 
 
 def load_logs(filename: str):
@@ -38,9 +53,27 @@ def load_logs(filename: str):
 
 @app.route('/1/queries/count/<date_prefix>', methods=['GET'])
 def count(date_prefix=None):
-    # TODO: add your code below...
+    if date_prefix is None:
+        return Response('Missing date prefix', status=400)
 
-    return jsonify({"count": 0})
+    matching_logs = []
+
+    if len(date_prefix) == 4:
+        matching_logs = [
+            log for log in logs if log["date"].strftime("%Y") == date_prefix]
+    elif len(date_prefix) == 7:
+        matching_logs = [log for log in logs if log["date"].strftime(
+            "%Y-%m") == date_prefix]
+    elif len(date_prefix) == 10:
+        matching_logs = [log for log in logs if log["date"].strftime(
+            "%Y-%m-%d") == date_prefix]
+    elif len(date_prefix) == 16:
+        matching_logs = [log for log in logs if log["date"].strftime(
+            "%Y-%m-%d, %H:%M") == date_prefix]
+    else:
+        return Response('Invalid date format. Supported formats: "YYYY", "YYYY-MM", "YYYY-MM-DD", "YYYY-MM-DD HH:MM"', status=400)
+
+    return jsonify({"count": len(matching_logs)})
 
 
 @app.route('/1/queries/popular/<date_prefix>', methods=['GET'])
@@ -49,11 +82,19 @@ def popular(date_prefix=None):
         return Response('Missing date prefix in request', status=400)
 
     size = request.args.get('size', type=int, default=3)
+    matchlog = [log for log in logs if log["date"].strftime(
+        "%Y-%m-%d").startswith(date_prefix)]
+
+    querycount = Counter(log["url"]for log in matchlog)
+
+    topquerry = querycount.most_common(size)
+
     json = {"queries": []}
+    for query, count in topquerry:
+        json["queries"].append({"query": query, "count": count})
 
-    # TODO: add your code below
+    
 
-    json["queries"].append({"query": date_prefix, "count": size})
     return jsonify(json)
 
 
